01918c86445b1f6910d7b002e81e7e1a12746ba4
doank15 <maidoan709@gmail.com>
2024-04-04 23:57:17 +0700

    feat: edit cart

--- Backend/src/v1/controllers/cart.js
+++ Backend/src/v1/controllers/cart.js
@@ -1,42 +1,41 @@
-const Cart = require('../models/cart');
-const FoodItem = require('../models/foodItem');
+const Cart = require("../models/cart");
+const FoodItem = require("../models/foodItem");
 const addToCart = async (req, res, next) => {
     try {
         const userID = req.user._id;
-        const {id} = req.params;
-        let {qty} = req.params;
-        const foodItem = await FoodItem.findById(id)
-        if(!foodItem) {
-            return res.status(404).json({message: "Food not found"});
+        const { id } = req.params;
+        let { qty } = req.params;
+        const foodItem = await FoodItem.findById(id);
+        if (!foodItem) {
+            return res.status(404).json({ message: "Food not found" });
         }
 
         const findUsersCart = await Cart.find({
             userID: userID,
             status: "pending",
-        })
+        });
 
         const existingCartItem = findUsersCart.find((eachItemInUseCart) => {
             return eachItemInUseCart.foodID.toString() === foodItem._id.toString();
-        })
+        });
 
-        if(existingCartItem) {
+        if (existingCartItem) {
             existingCartItem.quantity += parseInt(qty);
             await existingCartItem.save();
             const findCartItems = await Cart.find({
                 userID,
                 status: "pending",
             }).populate({
-                path: "foodID", 
-                select: "name image price"
-
+                path: "foodID",
+                select: "name image price",
             });
 
             return res.status(200).json({
                 success: true,
                 message: `${foodItem.name} quantity was updated to ${existingCartItem.quantity}`,
-                data: findCartItems
+                data: findCartItems,
             });
-        }else {
+        } else {
             if ((foodItem.id === id && qty <= 0) || null || undefined) {
                 qty = 1;
             }
@@ -45,7 +44,7 @@ const addToCart = async (req, res, next) => {
                     userID,
                     foodID: foodItem.id,
                     message: `${foodItem.name} quantity was updated to ${qty}`,
-                    quantity: qty
+                    quantity: qty,
                 });
 
                 const findCartItems = await Cart.find({
@@ -53,26 +52,24 @@ const addToCart = async (req, res, next) => {
                     status: "pending",
                 }).populate({
                     path: "foodID",
-                    select: "name image price"
+                    select: "name image price",
                 });
-                
+
                 return res.status(201).json({
                     success: true,
                     message: `${foodItem.name} was added to cart`,
-                    data: findCartItems
-                })
+                    data: findCartItems,
+                });
             }
         }
-        return res
-            .status(501)
-            .json({
-                message: `This isn't implemented as you have ${foodItem.name} in your cart before`
-            });
+        return res.status(501).json({
+            message: `This isn't implemented as you have ${foodItem.name} in your cart before`,
+        });
     } catch (error) {
         console.log(error.message);
-        next(error)
+        next(error);
     }
-}
+};
 
 const allCartItem = async (req, res, next) => {
     try {
@@ -82,18 +79,55 @@ const allCartItem = async (req, res, next) => {
             status: "pending",
         }).populate({
             path: "foodID",
-            select: "name image price"
-        })
+            select: "name image price",
+        });
 
         return res.status(200).json({
             success: true,
-            data: findUsersCart
-        })
+            data: findUsersCart,
+        });
     } catch (error) {
         next(error);
     }
-}
+};
+
+const editCart = async (req, res, next) => {
+    try {
+        const { foodItemForUpdate } = req.params;
+        const userID = req.user._id;
+        const newQuantity = req.params.qty;
+        const findCart = await Cart.findById(foodItemForUpdate);
+
+        console.log(findCart.userID);
+        if (findCart.userID.toString() === userID.toString() && findCart.quantity.toString() !== newQuantity.toString()) {
+            if (newQuantity <= 0 || null || undefined) {
+                return res.status(501).json({
+                    success: false,
+                    message: "Item cannot be less than one, so not implemented",
+                });
+            }
+
+            if (newQuantity >= 1) {
+                await findCart.updateOne({
+                    quantity: newQuantity,
+                });
+
+                return res.status(201).json({
+                    success: true,
+                    message: `${findCart.foodID} qty was updated to ${newQuantity}`,
+                });
+            }
+        }
+        return res.status(200).json({
+            success: true,
+            message: "in it really state nothing happened",
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 module.exports = {
     addToCart,
-    allCartItem
-}
+    allCartItem,
+    editCart
+};
--- Backend/src/v1/routes/cart.js
+++ Backend/src/v1/routes/cart.js
@@ -1,9 +1,10 @@
-const { addToCart, allCartItem } = require('../controllers/cart');
+const { addToCart, allCartItem, editCart } = require('../controllers/cart');
 const { verifyToken } = require('../middleware/tokenHandler');
 
 const router = require('express').Router();
 
 router.get("/", verifyToken, allCartItem)
 router.post("/:id/:qty", verifyToken, addToCart)
+router.put("/:foodItemForUpdate/qty/:qty", verifyToken, editCart)
 
 module.exports = router
