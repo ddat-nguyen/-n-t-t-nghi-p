0280b824d041a080a4940b4bd5fe14485ade694b
doank15 <maidoan709@gmail.com>
2024-05-14 02:29:56 +0700

    Refactor code

--- Frontend/src/pages/Dashboard/components/CircleChart.jsx
+++ Frontend/src/pages/Dashboard/components/CircleChart.jsx
@@ -9,105 +9,124 @@ import adminApi from "../../../api/admin";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const CircleChart = () => {
-    const option = ["Today", "Week", "All the time"];
-    const [orderOption, setOrderOption] = useState(option[0]);
-    const [order, setOrder] = useState([]);
+  const option = ["Today", "Week", "All the time"];
+  const [orderOption, setOrderOption] = useState(option[0]);
+  const [order, setOrder] = useState([]);
 
-    useEffect(() => {
-        const fetchOrder = async () => {
-            try {
-                const res = await adminApi.getOrderCountsByStatusThisWeek();
-                setOrder(res);
-                console.log(res);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchOrder();
-    }, [orderOption]);
-
-    const chartData = {
-        labels: order.map((item) => item.status),
-        datasets: [
-            {
-                data: order.map((item) => item.count),
-                backgroundColor: ["#8785E9", "#FFB572", "#50D1AA"],
-                borderColor: ["#8785E9", "#FFB572", "#50D1AA"],
-                borderWidth: 5,
-            },
-        ],
+  useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        const res = await adminApi.getOrderCountsByStatusThisWeek();
+        setOrder(res);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
     };
-    console.log(chartData)
-    return (
-        <div className="bg-base/dark-bg-2-14 dark:bg-light-bg-1 flex flex-col gap-5 min-h-[330px] p-6 rounded-lg">
-            <div className=" bg-base/dark-bg-2-14 dark:bg-light-bg-1 flex justify-between items-center">
-                <div className="text-xl font-semibold dark:text-dark">Most Ordered</div>
-                <MenuDropDown
-                    option={option}
-                    options={orderOption}
-                    setOptions={setOrderOption}
-                />
-            </div>
-            <hr className="h-[2px] bg-base/dark-line dark:bg-primary-color border-0" />
-            <div className="w-full flex justify-between items-center">
-                <div className="w-3/5">
-                    <Doughnut
-                        data={chartData}
-                        options={{
-                            plugins: {
-                                legend: {
-                                    display: false,
-                                },
-                            },
-                        }}
-                    />
-                </div>
-                <div className="flex flex-col gap-4 items-start">
-                    <div className="flex flex-row gap-2 w-20 items-start">
-                        <img
-                            src="https://file.rendit.io/n/rnZk21ZtZktYs2ulT5cL.svg"
-                            className="mt-px w-4 shrink-0"
-                        />
-                        <div className="flex flex-col gap-px w-16 shrink-0 items-start">
-                            <div className="text-sm font-medium dark:text-dark">Pending</div>
-                            <div className="text-xs text-gray-400 dark:text-slate-600">
-                                {order[0]?.count || 0} đơn hàng
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex flex-row mr-px gap-2 items-start">
-                        <img
-                            src="https://file.rendit.io/n/FjNeHzKAyZyJOxpk1vOX.svg"
-                            className="mt-px w-4 shrink-0"
-                        />
-                        <div className="flex flex-col gap-px w-20 shrink-0 items-start">
-                            <div className="text-sm font-medium dark:text-dark">
-                                Confirmed
-                            </div>
-                            <div className="text-xs text-gray-400 dark:text-slate-600">
-                                {order[1]?.count || 0} đơn hàng
-                            </div>
-                        </div>
-                    </div>
-                    <div className="relative flex flex-col pt-2 pb-8">
-                        <div
-                            className="w-24 h-12 absolute top-0 left-px flex flex-col gap-px items-start pl-4 py-1">
-                            <div className="text-sm font-medium ml-2 dark:text-dark">
-                                Delivered
-                            </div>
-                            <div className="text-xs text-gray-400 dark:text-slate-600 ml-2">
-                                {order[2]?.count || 0} đơn hàng
-                            </div>
-                        </div>
-                        <img
-                            src="https://file.rendit.io/n/TmvvFrtY5nlVB4WkgMUn.svg"
-                            className="relative w-4"
-                        />
-                    </div>
-                </div>
-            </div>
+    fetchOrder();
+  }, [orderOption]);
+
+  const chartData = {
+    labels: order.map((item) => item.status),
+    datasets: [
+      {
+        data: order.map((item) => item.count),
+        backgroundColor: order.map((item) => {
+          switch (item.status) {
+            case "confirmed":
+              return "#8785E9";
+            case "delivered":
+              return "#50D1AA";
+            case "pending":
+              return "#FFB572";
+            default:
+              return "#000000"; // Default color
+          }
+        }),
+        borderColor: order.map((item) => {
+          switch (item.status) {
+            case "confirmed":
+              return "#8785E9";
+            case "delivered":
+              return "#50D1AA";
+            case "pending":
+              return "#FFB572";
+            default:
+              return "#000000"; // Default color
+          }
+        }),
+        borderWidth: 5,
+      },
+    ],
+  };
+  console.log(chartData.datasets);
+  return (
+    <div className="bg-base/dark-bg-2-14 dark:bg-light-bg-1 flex flex-col gap-5 min-h-[330px] p-6 rounded-lg">
+      <div className=" bg-base/dark-bg-2-14 dark:bg-light-bg-1 flex justify-between items-center">
+        <div className="text-xl font-semibold dark:text-dark">Most Ordered</div>
+        <MenuDropDown
+          option={option}
+          options={orderOption}
+          setOptions={setOrderOption}
+        />
+      </div>
+      <hr className="h-[2px] bg-base/dark-line dark:bg-primary-color border-0" />
+      <div className="w-full flex justify-between items-center">
+        <div className="w-3/5">
+          <Doughnut
+            data={chartData}
+            options={{
+              plugins: {
+                legend: {
+                  display: false,
+                },
+              },
+            }}
+          />
         </div>
-    );
+        <div className="flex flex-col gap-4 items-start">
+          {order.map((item, index) => (
+            <div className="flex flex-row gap-2 items-start" key={index}>
+              <img
+                src={
+                  item.status === "confirmed"
+                    ? "https://file.rendit.io/n/rnZk21ZtZktYs2ulT5cL.svg"
+                    : item.status === "pending"
+                    ? "https://file.rendit.io/n/FjNeHzKAyZyJOxpk1vOX.svg"
+                    : "https://file.rendit.io/n/TmvvFrtY5nlVB4WkgMUn.svg"
+                }
+                className="mt-px w-4 shrink-0"
+              />
+              <div className="flex flex-col gap-px w-16 shrink-0 items-start">
+                <div
+                  className={`text-sm font-medium dark:text-dark ${
+                    item.status === "confirmed"
+                      ? "text-blue-500"
+                      : item.status === "pending"
+                      ? "text-yellow-500"
+                      : "text-green-500"
+                  }`}
+                >
+                  {item.status.charAt(0).toUpperCase() + item.status.slice(1)}
+                </div>
+                <div
+                  className={`text-xs text-gray-400 dark:text-slate-600 ${
+                    item.status === "confirmed"
+                      ? "text-blue-500"
+                      : item.status === "pending"
+                      ? "text-yellow-500"
+                      : "text-green-500"
+                  }`}
+                >
+                  {item.count} đơn hàng
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CircleChart;
